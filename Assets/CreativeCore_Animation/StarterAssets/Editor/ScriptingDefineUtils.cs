2c3148bdd4283444a9220a7d2e70102c05cd4ed6
vashi1lamba4-wwx <vashi1lamba4@gmail.com>
2026-04-24 09:30:11 +0200

    letsgo

---
+++ Assets/CreativeCore_Animation/StarterAssets/Editor/ScriptingDefineUtils.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEditor.Build;
+
+namespace StarterAssets
+{
+    public static class ScriptingDefineUtils
+    {
+        public static bool CheckScriptingDefine(string scriptingDefine)
+        {
+            NamedBuildTarget namedBuildTarget = NamedBuildTarget.FromBuildTargetGroup(BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget));
+            var defines = PlayerSettings.GetScriptingDefineSymbols(namedBuildTarget);
+            return defines.Contains(scriptingDefine);
+        }
+
+        public static void SetScriptingDefine(string scriptingDefine)
+        {
+            NamedBuildTarget namedBuildTarget = NamedBuildTarget.FromBuildTargetGroup(BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget));
+            var defines = PlayerSettings.GetScriptingDefineSymbols(namedBuildTarget);
+            if (!defines.Contains(scriptingDefine))
+            {
+                defines += $";{scriptingDefine}";
+                PlayerSettings.SetScriptingDefineSymbols(namedBuildTarget, defines);
+            }
+        }
+
+        public static void RemoveScriptingDefine(string scriptingDefine)
+        {
+            NamedBuildTarget namedBuildTarget = NamedBuildTarget.FromBuildTargetGroup(BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget));
+            var defines = PlayerSettings.GetScriptingDefineSymbols(namedBuildTarget);
+            if (defines.Contains(scriptingDefine))
+            {
+                string newDefines = defines.Replace(scriptingDefine, "");
+                PlayerSettings.SetScriptingDefineSymbols(namedBuildTarget, newDefines);
+            }
+        }
+    }
+}
